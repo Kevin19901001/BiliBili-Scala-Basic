07a81fc36b062e320fb3f86d708b22a3ef60576c
huanqing19901001@163.com <huanqing19901001@163.com>
2024-03-10 16:18:55 +0800

    Create Student.scala.

---
+++ chapter01/Student.scala
@@ -0,0 +1,23 @@
+package chapter01
+
+import chapter01.Student.school
+
+class Student (name: String, age: Int){
+
+  def printInfo(): Unit = {
+    println("name: " + name + "   " + "age: " + age + "   " + "school: " + school)
+  }
+
+}
+
+object Student{
+  var school = "clark school"
+
+  def main(args: Array[String]): Unit = {
+    val alice = new Student("alice", 23)
+    val bob = new Student("bob", 24)
+
+    alice.printInfo()
+    bob.printInfo()
+  }
+}
